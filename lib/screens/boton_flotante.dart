b8c7a6d6a120d80a9e6ebe51f81dde1f8853a763
LUIS_PABLO_CONTRERAS_SALOMON <a.20308051280997@cbtis128.edu.mx>
2023-05-18 21:38:53 +0000

    .

---
+++ lib/screens/boton_flotante.dart
@@ -0,0 +1,37 @@
+//Importacion de libreria material
+import 'package:flutter/material.dart';
+
+//Crear la clase y el estado al cambiar
+class BotonFlotante extends StatefulWidget {
+  const BotonFlotante({super.key});
+  _BotonFlotante createState() => _BotonFlotante();
+}
+
+//Crear los widgets a mostrar
+class _BotonFlotante extends State<BotonFlotante> {
+  String txt = "";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Boton Flotante'),
+      ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [Text(txt)],
+      )),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          FloatingActionButton(
+              child: Icon(Icons.visibility),
+              onPressed: () => setState(() => txt = "Hola Mundo")),
+          FloatingActionButton(
+              child: Icon(Icons.visibility_off),
+              onPressed: () => setState(() => txt = "")),
+        ],
+      ),
+    );
+  }
+}
